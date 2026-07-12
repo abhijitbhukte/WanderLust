cbe088de95ab6c11c3ef2c664b3269bc6499cce5
Abhijit Bhukte <abhijitbhukte85@gmail.com>
2026-07-13 00:57:34 +0530

    error handling middleware added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const expressError = require("./utils/expressError.js");
+const listingSchema = require("./schema.js");
 
 
 
@@ -29,6 +30,19 @@ main().then(() => {
 });
 
 
+// validation middleware
+const validateListing = (req, res, next) => {
+    const { error } = listingSchema.validate(req.body);
+
+    if (error) {
+        console.log(error.details);
+        let errMsg = error.details.map(el => el.message).join(", ");
+        throw new expressError(400, errMsg);
+    }
+
+    next();
+};
+
 // index route
 app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
@@ -43,11 +57,9 @@ app.get("/listings/new", wrapAsync(async (req, res) => {
 
 
 //add route
-app.post("/listings", wrapAsync(async (req, res) => {
+app.post("/listings",validateListing, wrapAsync(async (req, res) => {
     // {title, description, image, price, location, country} = req.body;
-    if(!req.body.listing) {
-        throw new expressError(400, "send valid data for listing");
-    }
+
     let newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -72,10 +84,7 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
 
 
 // update route
-app.put("/listings/:id", wrapAsync(async (req, res, next) => {
-    if(!req.body.listing) {
-        throw new expressError(400, "send valid data for listing");
-    }
+app.put("/listings/:id",validateListing, wrapAsync(async (req, res, next) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);  
@@ -96,13 +105,14 @@ app.get("/", (req, res) => {
 
 
 app.use((req, res, next) => {
-    next(new expressError(404, "Something wents worng"));
+    next(new expressError(404, "Page Not Found!"));
 });
 
 // error handling middleware
 app.use((err, req, res, next) => {
     let {status = 500, message = "Something wents Worng"} = err;
-    res.status(status).send(message);
+    res.status(status).render("./listings/error.ejs", {err});
+    // res.status(status).send(message);
 });
 
 app.listen(8080, () => {
---
+++ schema.js
@@ -0,0 +1,16 @@
+const Joi = require('joi');
+
+const listingSchema = Joi.object({
+    listing : Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        country: Joi.string().required(),
+        location: Joi.string().required(),
+        image:  Joi.object({
+            url: Joi.string().uri().allow("", null)
+        }).required()
+    }).required()
+});
+
+module.exports = listingSchema;
