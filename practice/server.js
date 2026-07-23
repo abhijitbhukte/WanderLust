1b8f199f776dbe1612c1a81cc40f7f1cc229a4a6
Abhijit Bhukte <abhijitbhukte85@gmail.com>
2026-07-24 00:47:53 +0530

    some packages and and one temporary folder are added

---
+++ practice/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+const session = require("express-session");
+app.use(session({secret: "secretstring",resave: false, saveUninitialized: true}));
+
+
+app.get("/register", (req, res) => {
+    let {name = "anonymous"} = req.query;
+    req.session.name = name;
+    console.log(req.session.name);
+    res.redirect("/hello");
+});
+
+
+app.get("/hello", (req, res) => {
+    res.send(`hello ${req.session.name}`);
+});
+
+
+// app.get("/reqcount", (req, res) => {
+//     if(req.session.count) {
+//         req.session.count++;
+//     } else {
+//         req.session.count = 1;
+//     }
+//     res.send(`You sent the request ${req.session.count} times`)
+// });
+
+
+
+
+app.listen(3000, () => {
+    console.log("Connection Successfully");
+});
